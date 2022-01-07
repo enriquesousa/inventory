28cae58c7dfec6512876931a794d17674a959cea
enriquesousa <enrique.sousa@gmail.com>
2022-01-06 22:17:42 -0800

    175. Project Login, Register, Forget, Password Page Setup

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Para poder visitar las paginas routes de vue
+Route::get('/{vue_capture?}',function () {
+    return view('welcome');
+})->where('vue_capture', '[\/\w\.-]*');
