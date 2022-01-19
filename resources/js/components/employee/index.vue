9118cd07165028b570255a62028ac096d2e2bd85
enriquesousa <enrique.sousa@gmail.com>
2022-01-18 20:07:54 -0800

    201. Read Data and Search Part 1

--- resources/js/components/employee/index.vue
+++ resources/js/components/employee/index.vue
@@ -1,49 +1,90 @@
 <template>
 <div>
+
     <div>
         <router-link to="/store-employee" class="btn btn-primary">Añadir Empleado</router-link>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-xl-12 col-lg-12 col-md-12">
-            <div class="card shadow-sm my-5">
-                <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="login-form">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Lista Empleados</h1>
-                                </div>
-                                
+    <br>
 
-
-
-
-
-
-                            </div>
-                        </div>
-                    </div>
+    <!-- Begin Row Simple Table -->
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <!-- Simple Tables -->
+            <div class="card">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Lista de Empleados</h6>
                 </div>
+                <div class="table-responsive">
+                    <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Foto</th>
+                                <th>Teléfono</th>
+                                <th>Salario</th>
+                                <th>Fecha Inicio</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr v-for="employee in employees" :key="employee.id">
+                                <td>{{ employee.name }}</td>
+                                <td> <img :src="employee.photo" id="em_photo"></td>
+                                <td>{{ employee.phone }}</td>
+                                <td>{{ employee.salary }}</td>
+                                <td>{{ employee.joining_date }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-primary">Editar</a>
+                                    <a href="#" class="btn btn-sm btn-danger">Borrar</a>
+                                </td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer"></div>
             </div>
         </div>
     </div>
+    <!-- End Row Simple Table -->
+
 </div>
 </template>
 
 <script type="text/javascript">
+export default {
 
-  export default {
+created(){
+    if (!User.loggedIn()){
+    this.$router.push({ name: 'home'})
+    }
+},
 
-    created(){
-      if (!User.loggedIn()){
-        this.$router.push({ name: 'home'})
-      }
-    },
-    
-  }
-  
+data(){
+    return{
+        employees:[]
+    }
+},
+
+methods:{
+    allEmployee(){
+        axios.get('/api/employee')
+        .then(({data}) => (this.employees = data))
+        .catch()
+    }
+},
+
+created(){
+    this.allEmployee();
+}
+
+}
 </script>
 
-<style>
-
+<style type="text/css">
+    #em_photo{
+        height: 40px;
+        width: 40px;
+    }
 </style>
